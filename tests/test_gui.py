b9b682a8fc131c781512bdec9cc66d4d71697e1d
Stranik <ondra.stranik@gmail.com>
2024-04-08 15:51:22 +0200

    Update test_gui.py

--- tests/test_gui.py
+++ tests/test_gui.py
@@ -19,5 +19,21 @@ def test_CameraGUI():
 
     camera.disconnect()
 
+def test_CameraViewGUI():
+    ''' check if gui works'''
+    from viscope.gui.cameraViewGUI import CameraViewGUI
+    from viscope.instrument.virtual.virtualCamera import VirtualCamera
+    from viscope.main import Viscope
+
+    camera = VirtualCamera(name='camera1')
+    camera.connect()
+    camera.setParameter('threadingNow',True)
+
+    viscope = Viscope()
+    newGUI  = CameraViewGUI(viscope)
+    newGUI.setDevice(camera)
+    viscope.run()
+
+    camera.disconnect()
 
 
